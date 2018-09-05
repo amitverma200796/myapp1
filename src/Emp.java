266ef1afccb55d572fba56953d2db2c917fe9efc
Amit <Amit@Amit-PC>
2018-09-05 10:42:51 -0700

    new update

---
+++ src/Emp.java
@@ -0,0 +1,23 @@
+
+
+public class Emp
+{
+private String  ename;
+public  String sal;
+
+public String getSal() {
+	return sal;
+}
+
+public void setSal(String sal) {
+	this.sal = sal;
+}
+
+public String getEname() {
+	return ename;
+}
+
+public void setEname(String ename) {
+	this.ename = ename;
+}
+}
